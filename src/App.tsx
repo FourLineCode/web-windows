006641bf3d9bb9cce7e3ec0eebdd4b7e677c6cc0
ItzAkmal <akmal3535.ah@gmail.com>
2021-03-31 17:47:09 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function App() {
+	return (
+		<main className='flex flex-col items-center justify-center w-screen h-screen bg-gray-700'>
+			<div className='font-mono text-6xl font-bold text-gray-300'>Web Windows X</div>
+			<div className='font-mono text-3xl font-bold text-gray-300'>- Akmal Hossain</div>
+		</main>
+	);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	purge: ['./src/**/*.ts', './src/**/*.tsx'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
